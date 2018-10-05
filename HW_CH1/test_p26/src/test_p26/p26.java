7bd9fe0ea34fd2ceb3812c859cfe6d3735a0a432
ian9889 <ian570623@yahoo.com.tw>
2018-10-05 16:41:29 +0800

    HW_CH1

---
+++ HW_CH1/test_p26/src/test_p26/p26.java
@@ -0,0 +1,13 @@
+package test_p26;
+
+public class p26 {
+	public static void main(String[] args){
+		int num=3;
+		System.out.println("跑计num琌"+num);
+		
+		num=5;
+		
+		System.out.println("穝跑计num");
+		System.out.println("跑计num穝计"+num);
+	}
+}
