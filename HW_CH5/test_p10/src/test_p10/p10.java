daab2fc28c74b0d997ab72952d7d38d4916e2b42
ian9889 <ian570623@yahoo.com.tw>
2018-11-27 11:10:20 +0800

    hw5

---
+++ HW_CH5/test_p10/src/test_p10/p10.java
@@ -0,0 +1,9 @@
+package test_p10;
+
+public class p10 {
+	public static void main(String[] args){
+		pc.Car car1;
+		car1=new pc.Car();
+		car1.show();
+	}
+}
---
+++ HW_CH5/test_p14/src/test_p14/p14.java
@@ -0,0 +1,9 @@
+package test_p14;
+import pc.Car;
+
+public class p14 {
+	public static void main(String[] args){
+		Car car1=new Car();
+		car1.show();
+	}
+}
